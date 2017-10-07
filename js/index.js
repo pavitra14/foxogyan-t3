f704b28eda01ff1922e97dd02426891213a106fc
Pavitra Behre <pavitra.behre@gmail.com>
2017-10-07 19:49:59 +0530

    Added fadeIn() effect to the page content

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 **Date Created : 6th October 2017
 **Author : Pavitra Behre(me@pbehre.in)
 */
+//The following functions are to fix the "active" focus and fade in effect on the carousel div
 function slide1() {
     console.log('slide1() triggered');
     var s1 = $(".slide1");
@@ -18,18 +19,27 @@ function slide2() {
     var s1 = $(".slide1");
     var s2 = $(".slide2");
     var s3 = $(".slide3");
+    var h2 = $(".hidden2");
     s1.removeClass("active");
     s2.removeClass("active"); //remove the class if already applied
     s3.removeClass("active");
     s2.addClass("active");
+    h2.fadeIn(1000).removeClass("hidden2");
 }
 function slide3() {
     console.log('slide3() triggered');
     var s1 = $(".slide1");
     var s2 = $(".slide2");
     var s3 = $(".slide3");
+    var h3 = $(".hidden3");
     s1.removeClass("active");
     s2.removeClass("active");
     s3.removeClass("active"); //remove the class if already applied
     s3.addClass("active");
+    h3.fadeIn(1000).removeClass("hidden3");
 }
+
+//Document ready code goes here
+$(document).ready(function () {
+    $('.hidden1').fadeIn(1500).removeClass('hidden1'); //1500 cause first run, this line causes the fade in effect of the content when the user first navigates through the site
+});
